c9630b47ce1c28a4a0fcedcc7f06c6605ba21f26
soojwang <soojwang@safaricom.co.ke>
2025-04-21 07:01:35 +0300

    implemented map, set, arraylist and records to implements a ticket booking system

--- src/main/java/collections/CollectionsDemo.java
+++ src/main/java/collections/CollectionsDemo.java
@@ -15,10 +15,16 @@ public class CollectionsDemo {
         fruits.add(new Fruit("Apple", 40));
         fruits.add(new Fruit("Mango", 20));
         fruits.add(new Fruit("Watermelon", 200));
+        fruits.set(1, new Fruit("Pineaple", 150));
         System.out.println(fruits);
 
         return fruits;
     }
 
-    public record Fruit(String name, int price){};
+    public record Fruit(String name, int price){
+        @Override
+        public String toString() {
+            return name + "(KES " + price + ")";
+        }
+    };
 }
